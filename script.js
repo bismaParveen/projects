de1936ceab3a93253a0185ed04c614810e23bafc
bismaParveen <145197980+bismaParveen@users.noreply.github.com>
2023-09-28 13:51:42 +0530

    cardhovereffect

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-const ourItemDiv = document.getElementsByClassName("item");
-const openIcon = document.getElementsByClassName("iconsOpen");
-const closeIcon = document.getElementsByClassName("iconsClose");
-// console.log(ourItemDiv);
-for (let i = 0; i < ourItemDiv.length; i++) {
-  closeIcon[i].style.display = "none";
-  ourItemDiv[i].addEventListener("click", () => {
-    const result = ourItemDiv[i].classList.toggle("active");
-    if (result) {
-      closeIcon[i].style.display = "block";
-      openIcon[i].style.display = "none";
-    } else {
-      closeIcon[i].style.display = "none";
-      openIcon[i].style.display = "block";
-    }
-  });
-}
+let pupils = document.getElementsByClassName("pupil");
+let pupilsArr = Array.from;
+
+// pupils movement
+let pupilStartPoint = -100;
+let pupilRange = 200;
+
+// mouse X position
+let mouseXStartPoint = 300;
+let mouseXEndPoint = window.innerWidth - 300;
+let currentPosition = 0;
+let fracXValue = 0;
+
+// mouse Y position
+let mouseYEndPoint = window.innerHeight;
+let currentYPosition = 0;
+let fracYValue = 0;
+
+let mouseXRange = mouseXEndPoint - mouseXStartPoint;
+
+const mouseMove = (event) => {
+  currentXPosition = event.clientX - mouseXStartPoint;
+  fracXValue = currentXPosition / mouseXRange;
+
+  currentYPosition = event.clientY;
+  fracYValue = currentYPosition / mouseYEndPoint;
+};
+
+window.addEventListener("mousemove", mouseMove);
